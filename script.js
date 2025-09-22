b9a371b825a38830f160d82638c5f677ddd05142
SanaUchida <sanacchi817@gmail.com>
2025-09-22 21:33:32 +0900

    renewed

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert("サイトへようこそ！")
+
+function showContent(id, element) {
+    document.querySelectorAll('.content-section').forEach(el => {
+      el.classList.remove('active');
+    });
+    document.getElementById(id).classList.add('active');
+
+    document.querySelectorAll('#sidebar a').forEach(el => {
+      el.classList.remove('active');
+    });
+    element.classList.add('active');
+}
